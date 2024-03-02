988b7000ea6b32a163a5186d0a87ff5457b1ab42
hosseinj13 <hosseinj13@yahoo.com>
2024-03-02 16:31:09 +0330

    Add filter() method to the Main.java from Stream.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,12 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<Person> persons = MockData.getPeople();
-        
+
+        //List<Person> personsOver50 = persons.stream().filter(person -> person.getAge() > 50).collect(Collectors.toList());
+        List<Person> personsOver50 = persons.stream().filter(person -> person.getAge() > 50).toList();
+
+        System.out.println("Persons over 50 years old:");
+        personsOver50.forEach(System.out::println);
 
     }
 }
