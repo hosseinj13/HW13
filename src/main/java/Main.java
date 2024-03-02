1f661c51ffd9877b0a1cead0bbba54753d24f51f
hosseinj13 <hosseinj13@yahoo.com>
2024-03-02 16:51:42 +0330

    Add personsOver50() method.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,23 @@
 import mockdata.MockData;
 import model.Person;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
-    public static void main(String[] args) {
-        List<Person> persons = MockData.getPeople();
-
+    static final List<Person> persons = MockData.getPeople();
+    public static void personsOver50(){
         //List<Person> personsOver50 = persons.stream().filter(person -> person.getAge() > 50).collect(Collectors.toList());
-        List<Person> personsOver50 = persons.stream().filter(person -> person.getAge() > 50).toList();
+       assert persons != null;
+       List<Person> personsOver50 = persons.stream().filter(person -> person.getAge() > 50).toList();
+      System.out.println("Persons over 50 years old:");
+       personsOver50.forEach(System.out::println);
+    }
 
-        System.out.println("Persons over 50 years old:");
-        personsOver50.forEach(System.out::println);
+
+    public static void main(String[] args) {
+        personsOver50();
 
     }
 }
