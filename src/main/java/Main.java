6eeb3e1e1ebdaee9691ea0a64fcf5744646a169e
hosseinj13 <hosseinj13@yahoo.com>
2024-03-02 16:28:49 +0330

    Add getPeople() method from MockData.java

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import mockdata.MockData;
+import model.Person;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Person> persons = MockData.getPeople();
+        
+
+    }
+}
